1a24af686828688ef03e8d6e777696daed40a674
Yiman Deng <109169076+YimanDeng@users.noreply.github.com>
2023-02-03 22:16:27 -0800

    Update binary_search.cpp

--- binary_search/binary_search.cpp
+++ binary_search/binary_search.cpp
@@ -32,3 +32,29 @@ int main() {
 
     return 0;
 }
+
+/* 
+Closest to the left/right
+This is the implementation for closest to l
+Idea: maintain invariant l <= q and r > q;
+*/
+int main() {
+    int n, k; cin >> n >> k;
+    vector<int> a(n);
+    for (int i=0; i<n; i++) cin >> a[i];
+
+    for (int i=0; i<k; i++) {
+        int q; cin >> q;
+        int l = -1, r = n;
+        bool ok = false;
+        while (l+1<r) {
+            int m = (l+r) / 2;
+            if (q < a[m]) r = m; // <= for r
+            else if (q >= a[m]) l = m; // > for r
+        }
+        // a better way to implement binary search, just add check if l == q
+        cout << l << endl; // beware if problem statement uses 1-indexing
+    }
+
+    return 0;
+}
