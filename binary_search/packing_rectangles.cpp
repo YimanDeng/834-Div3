2d7f12db50d1b04ec98c0719d98216288d9554d2
Yiman Deng <109169076+YimanDeng@users.noreply.github.com>
2023-02-03 22:19:55 -0800

    Create packing_rectangles.cpp

---
+++ binary_search/packing_rectangles.cpp
@@ -0,0 +1,36 @@
+/*
+input: rectangle of width w and length h. n of them
+
+output: minimum side length of square such that the all rectangles fit
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+typedef long long ll;
+
+ll w, h, n;
+
+/* 
+calculate how many rectangles can a square of side length m fit
+compare that number with the number needed
+*/
+bool isGood(ll m) {
+    return (m/w) * (m/h) >= n;
+}
+
+int main() {
+    cin >> w >> h >> n;
+
+    ll l = 0, r = 1;
+    while (!isGood(r)) r *= 2;
+
+    while (l+1<r) {
+        ll m = (l+r)/2;
+        if (isGood(m)) r = m;
+        else l = m;
+    }
+
+    cout << r << endl;
+
+    return 0;
+}
