3e6d19424b27154e75341fa5791dd99fd3313bbe
Yiman Deng <109169076+YimanDeng@users.noreply.github.com>
2023-02-04 21:25:50 -0800

    Create templet.cpp

---
+++ templet.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define print(x) cerr << #x << " is " << x << endl;
+
+void solve() {
+
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+  
+    solve();
+  
+    return 0;
+}
